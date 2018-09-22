9fe5a1313677f2735b21c3b6ce255528ee7c559e
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 10:38:41 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan13/relational/PBO210117049Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan13.relational;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan operasi
+ *                        bilangan dengan operator relational
+ */
+public class PBO210117049Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = "+ (bil1 == bil2));
+        System.out.println("a != b = "+ (bil1 != bil2));
+        System.out.println("a > b = "+ (bil1 > bil2));
+        System.out.println("a < b = "+ (bil1 < bil2));
+        System.out.println("b >= a = "+ (bil2 >= bil1));
+        System.out.println("b <= a = "+ (bil2 <= bil1));
+    }
+    
+}
